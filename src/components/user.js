39a73c969fbeab44110f9650dd4ff1c60bb4f118
Mert <107802530+MertOzkan35@users.noreply.github.com>
2022-10-24 16:32:21 +0300

    responsive fix

--- src/components/user.js
+++ src/components/user.js
@@ -39,7 +39,7 @@ function User() {
               )}
             </div>
             <button
-              className="w-1/2 h-12 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className=" w-full md:w-1/2 h-12 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
               onClick={cv}
             >
               {" "}
